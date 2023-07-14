ca7170dd0b768f5752af6cc51a3c0fc446ff5eea
Zafeer Mahmood <zafeer7460@gmail.com>
2023-07-14 20:16:20 +0500

    changed idea2

--- idea2.py
+++ idea2.py
@@ -1,30 +1,25 @@
 import cv2
 import mediapipe as mp
 import numpy as np
+
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
-
-cap = cv2.VideoCapture('bike1.mp4')
-
-
 def calculate_angle(a, b, c):
-    a = np.array(a)  # First
-    b = np.array(b)  # Mid
-    c = np.array(c)  # End
+    a = np.array(a)  # First point
+    b = np.array(b)  # Mid point
+    c = np.array(c)  # End point
 
-    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - \
-        np.arctan2(a[1]-b[1], a[0]-b[0])
-    angle = np.abs(radians*180.0/np.pi)
+    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
+    angle = np.abs(radians * 180.0 / np.pi)
 
     if angle > 180.0:
-        angle = 360-angle
+        angle = 360 - angle
 
     return angle
 
-
-# Setup mediapipe instance
 cap = cv2.VideoCapture('bike1.mp4')
+
 # Setup mediapipe instance
 with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
     while cap.isOpened():
@@ -46,33 +41,32 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
             landmarks = results.pose_landmarks.landmark
 
             # Get coordinates
-            
             shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
                         landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
             hip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,
-                     landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
+                   landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
             knee = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x,
-                     landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]            
+                    landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]
+
             # Calculate angle
             angle = calculate_angle(shoulder, hip, knee)
 
             # Visualize angle
-            cv2.putText(image, str(angle),
-                        tuple(np.multiply(hip, [640, 480]).astype(int)),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,
-                                                        255, 255), 2, cv2.LINE_AA
-                        )
+            cv2.putText(image, f'Angle: {angle:.2f} degrees',
+                        (30, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+            # Check if back is straight or bent down
+            if angle < 160:
+                cv2.putText(image, 'Warning: Back not straight!',
+                            (30, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
 
         except:
             pass
 
         # Render detections
         mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                  mp_drawing.DrawingSpec(
-                                      color=(245, 117, 66), thickness=2, circle_radius=2),
-                                  mp_drawing.DrawingSpec(
-                                      color=(245, 66, 230), thickness=2, circle_radius=2)
-                                  )
+                                  mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
+                                  mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2))
 
         cv2.imshow('Mediapipe Feed', image)
 
@@ -82,6 +76,6 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
         # Only continue iterating to the next frame if the user presses 'w'
         while cv2.waitKey(10) & 0xFF != ord('w'):
             pass
-        
-    cap.release()
-    cv2.destroyAllWindows()
+
+cap.release()
+cv2.destroyAllWindows()
